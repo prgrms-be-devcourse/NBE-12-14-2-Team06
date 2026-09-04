7c3c0be984750b019880ac3efb02e21ebb96c00b
kimssin1991 <kimsssin1991@naver.com>
2026-09-04 15:53:32 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/back/nbe12142team06/Nbe12142Team06Application.java
@@ -0,0 +1,13 @@
+package com.back.nbe12142team06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe12142Team06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe12142Team06Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/nbe12142team06/Nbe12142Team06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.nbe12142team06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe12142Team06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
