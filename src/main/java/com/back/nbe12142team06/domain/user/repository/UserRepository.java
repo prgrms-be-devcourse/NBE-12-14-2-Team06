7fa3ff8edffd08f3ccdd968d3437e11c30cde186
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-09-10 23:04:27 +0900

    test: 회원가입 이미 존재하는 아이디로 가입 시 예외 처리 (GREEN)

--- src/main/java/com/back/nbe12142team06/domain/user/repository/UserRepository.java
+++ src/main/java/com/back/nbe12142team06/domain/user/repository/UserRepository.java
@@ -7,5 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // username으로 튜플 검색
     Optional<User> findByUsername(String username);
+
+    // DB에 username이 존재하는지 확인
+    boolean existsByUsername(String username);
 }
--- src/main/java/com/back/nbe12142team06/domain/user/service/UserService.java
+++ src/main/java/com/back/nbe12142team06/domain/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.back.nbe12142team06.domain.user.entity.User;
 import com.back.nbe12142team06.domain.user.enums.Gender;
 import com.back.nbe12142team06.domain.user.enums.Role;
 import com.back.nbe12142team06.domain.user.repository.UserRepository;
+import com.back.nbe12142team06.global.exception.DuplicatedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,11 @@ public class UserService {
 
     @Transactional
     public User signUp(String username, String password, String email, String name, Role role, Gender gender, LocalDate birthDate, String phoneNumber, String region) {
+        // username 중복 검사
+        if (this.userRepository.existsByUsername(username)) {
+            throw new DuplicatedException("이미 사용 중인 아이디입니다.");
+        }
+
         User user = new User(username, password, email, name, role, gender, birthDate, phoneNumber, region);
         return this.userRepository.save(user);
     }
