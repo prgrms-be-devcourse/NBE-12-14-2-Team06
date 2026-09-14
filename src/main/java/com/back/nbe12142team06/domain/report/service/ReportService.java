ecb6212e829a588271530a0d75319480032d9d9b
globaltoper <globaltoper2@gmail.com>
2026-09-14 17:12:07 +0900

    feat: 진료 보고서 작성 기능 구현 및 테스트 추가

---
+++ src/main/java/com/back/nbe12142team06/domain/report/service/ReportService.java
@@ -0,0 +1,37 @@
+package com.back.nbe12142team06.domain.report.service;
+
+import com.back.nbe12142team06.domain.application.entity.Application;
+import com.back.nbe12142team06.domain.application.repository.ApplicationRepository;
+import com.back.nbe12142team06.domain.report.dto.ReportWriteRequest;
+import com.back.nbe12142team06.domain.report.entity.Report;
+import com.back.nbe12142team06.domain.report.repository.ReportRepository;
+import com.back.nbe12142team06.global.exception.DuplicatedException;
+import com.back.nbe12142team06.global.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReportService {
+
+    private final ReportRepository reportRepository;
+    private final ApplicationRepository applicationRepository;
+
+    public Report write(Long applicationId, ReportWriteRequest request) {
+
+        Application application = applicationRepository.findById(applicationId)
+                .orElseThrow(() -> new NotFoundException(1, "존재하지 않는 동행 건입니다."));
+
+        if (reportRepository.existsByApplicationId(applicationId)) {
+            throw new DuplicatedException(1, "이미 보고서가 작성된 동행 건입니다.");
+        }
+
+        return reportRepository.save(
+                Report.builder()
+                        .application(application)
+                        .title(request.title())
+                        .originContent(request.originContent())
+                        .build()
+        );
+    }
+}
