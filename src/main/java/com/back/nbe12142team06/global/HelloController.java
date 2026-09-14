d94485e2407e4b024fe5a22d8b7da11aa30141f7
kimssin1991 <kimsssin1991@naver.com>
2026-09-14 14:45:00 +0900

    chore : @Restcontroller 추가

--- src/main/java/com/back/nbe12142team06/global/HelloController.java
+++ src/main/java/com/back/nbe12142team06/global/HelloController.java
@@ -1,7 +1,10 @@
 package com.back.nbe12142team06.global;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
     @GetMapping("/")
     public String hello() {
