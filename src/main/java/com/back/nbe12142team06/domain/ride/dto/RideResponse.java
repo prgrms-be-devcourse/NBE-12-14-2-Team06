69675cd0fe18e2519baa83f0ba3d9ef781d481f7
PHH1123 <gsjh2001@koreatech.ac.kr>
2026-09-16 09:07:29 +0900

    refactor: 이동 API 응답 DTO에 주소 추가

--- src/main/java/com/back/nbe12142team06/domain/ride/dto/RideResponse.java
+++ src/main/java/com/back/nbe12142team06/domain/ride/dto/RideResponse.java
@@ -15,26 +15,34 @@ public record RideResponse(
         RideSelect selected,
         BigDecimal originLat,
         BigDecimal originLnt,
+        String originAddr,
         BigDecimal destLat,
-        BigDecimal destLnt) {
+        BigDecimal destLnt,
+        String destAddr) {
     public RideResponse(Ride ride) {
         Post post = ride.getPost();
         BigDecimal originLat = null;
         BigDecimal originLnt = null;
+        String originAddr = null;
         BigDecimal destLat = null;
         BigDecimal destLnt = null;
+        String destAddr = null;
         if (ride.getDirection().equals(RideDirection.TO_HOSPITAL)) {
             originLat = post.getPickupLat();
             originLnt = post.getPickupLng();
+            originAddr = post.getPickupAddress();
             destLat = post.getHospitalLat();
             destLnt = post.getHospitalLng();
+            destAddr = post.getHospitalAddress();
         } else if (ride.getDirection().equals(RideDirection.TO_HOME)) {
             originLat = post.getHospitalLat();
             originLnt = post.getHospitalLng();
+            originAddr = post.getHospitalAddress();
             destLat = post.getPickupLat();
             destLnt = post.getPickupLng();
+            destAddr = post.getPickupAddress();
         }
         this(ride.getId(), ride.getDirection(), ride.getRideStatus(), ride.getSelected(),
-                originLat, originLnt, destLat, destLnt);
+                originLat, originLnt, originAddr, destLat, destLnt, destAddr);
     }
 }
