6c924688b3e629e2cfa33f06f6d63c8a9576e0c1
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-09-17 10:28:45 +0900

    test: [USerController] username 중복 검사 - 이미 존재하는 username은 false (GREEN)

--- src/test/java/com/back/nbe12142team06/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/back/nbe12142team06/domain/user/controller/UserControllerTest.java
@@ -504,7 +504,8 @@ public class UserControllerTest {
     @DisplayName("[UserController] Username 중복 검사 - 사용 가능한 username은 true")
     void t15() throws Exception {
         ResultActions resultActions = mvc.perform(
-                get("/api/v1/users/username?username=user1")
+                get("/api/v1/users/username")
+                        .param("username", "user1")
         )
         .andDo(print());
 
@@ -513,5 +514,41 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.statusCode").value("200-2"))
                 .andExpect(jsonPath("$.msg").value("사용 가능한 아이디입니다."))
                 .andExpect(jsonPath("$.data").value("true"));
+    }
+
+    @Test
+    @DisplayName("[UserController] Username 중복 검사 - 이미 존재하는 username은 false")
+    void t16() throws Exception {
+        String signUpBody = """
+        {
+            "username": "user1",
+            "password": "pwd1",
+            "email": "first@test.test",
+            "name": "김춘식",
+            "role": "CLIENT",
+            "gender": "MALE",
+            "birthDate": "1990-05-20",
+            "phoneNum": "010-1234-5678",
+            "region": "서울시"
+        }
+        """;
+
+        // 회원 가입
+        mvc.perform(post("/api/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signUpBody))
+                .andDo(print());
+
+        ResultActions resultActions = mvc.perform(
+                        get("/api/v1/users/username")
+                                .param("username", "user1")
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value("200-2"))
+                .andExpect(jsonPath("$.msg").value("이미 사용중인 아이디입니다."))
+                .andExpect(jsonPath("$.data").value("false"));
     }
 }
