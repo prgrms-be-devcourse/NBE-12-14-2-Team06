a0205d19371a306c8a623a3041ce96161142d28e
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-09-21 11:46:39 +0900

    refactor: 프로필 관련 레포지토리에 Modifying 애너테이션 추가

--- src/main/java/com/back/nbe12142team06/domain/user/repository/ClientProfileRepository.java
+++ src/main/java/com/back/nbe12142team06/domain/user/repository/ClientProfileRepository.java
@@ -2,9 +2,12 @@ package com.back.nbe12142team06.domain.user.repository;
 
 import com.back.nbe12142team06.domain.user.entity.ClientProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ClientProfileRepository extends JpaRepository<ClientProfile, Integer> {
+
+    @Modifying
     @Query("delete from ClientProfile p where p.user.id = :userId")
     void deleteByUserId(Long userId);
 }
--- src/main/java/com/back/nbe12142team06/domain/user/repository/EscortProfileRepository.java
+++ src/main/java/com/back/nbe12142team06/domain/user/repository/EscortProfileRepository.java
@@ -2,9 +2,12 @@ package com.back.nbe12142team06.domain.user.repository;
 
 import com.back.nbe12142team06.domain.user.entity.EscortProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface EscortProfileRepository extends JpaRepository<EscortProfile, Long> {
+
+    @Modifying
     @Query("delete from EscortProfile p where p.user.id = :userId")
     void deleteByUserId(Long userId);
 }
