a1eb1e9d8ee54d668bd4ee47f921254262a601cf
lion1230 <skgmldnjs8080@gmail.com>
2026-09-22 15:29:11 +0900

    feat: review Swagger 문서화

--- src/main/java/com/back/nbe12142team06/domain/review/controller/ReviewController.java
+++ src/main/java/com/back/nbe12142team06/domain/review/controller/ReviewController.java
@@ -6,11 +6,14 @@ import com.back.nbe12142team06.domain.review.entity.Review;
 import com.back.nbe12142team06.domain.review.service.ReviewService;
 import com.back.nbe12142team06.global.response.RsData;
 import com.back.nbe12142team06.global.security.SecurityUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "리뷰", description = "동행 리뷰 작성 및 조회 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/applications")
@@ -18,7 +21,11 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    // 리뷰 작성
+    @Operation(
+            summary = "동행 리뷰 작성",
+            description = "특정 동행 건에 대한 리뷰를 작성합니다."
+    )
+
     @PostMapping("/{applicationId}/reviews")
     public RsData<ReviewWriteResponse> write(
             @PathVariable Long applicationId,
--- src/main/java/com/back/nbe12142team06/domain/review/controller/UserReviewController.java
+++ src/main/java/com/back/nbe12142team06/domain/review/controller/UserReviewController.java
@@ -3,6 +3,8 @@ package com.back.nbe12142team06.domain.review.controller;
 import com.back.nbe12142team06.domain.review.dto.ReviewDto;
 import com.back.nbe12142team06.domain.review.service.ReviewService;
 import com.back.nbe12142team06.global.response.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "리뷰", description = "동행 리뷰 작성 및 조회 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
@@ -18,7 +21,10 @@ public class UserReviewController {
 
     private final ReviewService reviewService;
 
-    // 특정 동행인이 받은 리뷰 목록 조회
+    @Operation(
+            summary = "동행자 리뷰 목록 조회",
+            description = "특정 동행자가 받은 리뷰 목록을 조회합니다."
+    )
     // 리뷰는 동행인 프로필에 공개되는 정보이므로 별도 권한 체크 없음
     @GetMapping("/{userId}/reviews")
     public RsData<List<ReviewDto>> list(@PathVariable Long userId) {
