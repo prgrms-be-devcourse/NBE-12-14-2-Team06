2c6d4e2fe300120c92e970e2502991e2aaec19ab
lion1230 <skgmldnjs8080@gmail.com>
2026-09-26 21:04:20 +0900

    fix: 취소된 지원 지원자 목록에서 제외

--- src/main/java/com/back/nbe12142team06/domain/application/repository/ApplicationRepository.java
+++ src/main/java/com/back/nbe12142team06/domain/application/repository/ApplicationRepository.java
@@ -18,21 +18,23 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
 
     // 공고별 지원 목록 조회
     @Query("""
-        SELECT a
-        FROM Application a
-        JOIN FETCH a.escort
-        WHERE a.post.id = :postId
-        """)
+    SELECT a
+    FROM Application a
+    JOIN FETCH a.escort
+    WHERE a.post.id = :postId
+      AND a.status <> com.back.nbe12142team06.domain.application.enums.ApplicationStatus.CANCELED
+    """)
     List<Application> findAllByPostIdWithEscort(@Param("postId") Long postId);
 
     // 페이징 조회용
     @Query("""
-        SELECT a
-        FROM Application a
-        JOIN FETCH a.escort
-        WHERE a.post.id = :postId
-        ORDER BY a.id DESC
-        """)
+    SELECT a
+    FROM Application a
+    JOIN FETCH a.escort
+    WHERE a.post.id = :postId
+      AND a.status <> com.back.nbe12142team06.domain.application.enums.ApplicationStatus.CANCELED
+    ORDER BY a.id DESC
+    """)
     Page<Application> findAllByPostIdWithEscort(
             @Param("postId") Long postId,
             Pageable pageable
